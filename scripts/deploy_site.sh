20fb2f095adba9fa11f3041d3701d5757a15e108
Jesse Squires <jesse@jessesquires.com>
2017-07-23 21:07:54 -0700

    update site deploy script

--- scripts/deploy_site.sh
+++ scripts/deploy_site.sh
@@ -8,9 +8,17 @@ PUBLIC_WWW=/home/public
 
 echo 'Pulling latest changes and building site...'
 cd $SITE_CHECKOUT
+
+echo ''
 git --git-dir=$GIT_DIR status
+
+echo ''
 git --git-dir=$GIT_DIR pull -f
+
+echo ''
 git --git-dir=$GIT_DIR status
+
+echo ''
 bundle install
 bundle exec jekyll build --destination $PUBLIC_WWW
 
