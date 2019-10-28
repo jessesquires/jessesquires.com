8b78c9da02adfddc25dedbca48b7ca2a4274bf36
Jesse Squires <jesse@jessesquires.com>
2019-10-28 11:03:03 -0700

    update deloy script

--- scripts/deploy_site.sh
+++ scripts/deploy_site.sh
@@ -9,22 +9,22 @@ GIT_DIR=$SITE_CHECKOUT/.git
 PUBLIC_WWW=/home/public
 
 echo 'Pulling latest changes and building site...'
-cd $SITE_CHECKOUT
+( set -x; cd $SITE_CHECKOUT )
 
 echo ''
-git --git-dir=$GIT_DIR status
+( set -x; git --git-dir=$GIT_DIR status )
 
 echo ''
-git --git-dir=$GIT_DIR pull -f
+( set -x; git --git-dir=$GIT_DIR pull -f )
 
 echo ''
-git --git-dir=$GIT_DIR status
+( set -x; git --git-dir=$GIT_DIR status )
 
 echo ''
-bundle install
+( set -x; bundle install )
 
 echo ''
-bundle exec jekyll build --destination $PUBLIC_WWW
+( set -x; bundle exec jekyll build --destination $PUBLIC_WWW )
 
 echo ''
 echo 'Done! Site built and deployed successfully.'
