5c2bc735c880378438945cdeb7374967322cf080
Jesse Squires <jesse@jessesquires.com>
2020-05-22 20:04:08 -0700

    Link to specific categories
    Refactor how main blog page works.
    
    - no longer using bootstrap `tablist` / `nav-tab` / `tab-content`
    
    - each category becomes its own page at `/blog/CATEGORY_NAME` using custom generator
    
    - for post entries, link to specific category page
    
    closes #100

---
+++ _plugins/category-page-generator.rb
@@ -0,0 +1,31 @@
+# Custom generator for category pages
+# Based on: https://jekyllrb.com/docs/plugins/generators/
+
+module Jekyll
+  class CategoryPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'category_index'
+        dir = 'blog'
+        site.categories.each_key do |category|
+          site.pages << CategoryPage.new(site, site.source, File.join(dir, category), category)
+        end
+      end
+    end
+  end
+
+  # A Page subclass used in the `CategoryPageGenerator`
+  class CategoryPage < Page
+    def initialize(site, base, dir, category)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+      self.data['category'] = category
+    end
+  end
+end
