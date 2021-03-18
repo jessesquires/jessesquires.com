02ff22dc80d585cd57382608e9929beb41a6da32
Jesse Squires <jesse@jessesquires.com>
2021-03-18 10:43:03 -0700

    update date script to pbcopy

--- scripts/current-date-time.swift
+++ scripts/current-date-time.swift
@@ -1,6 +1,7 @@
 #!/usr/bin/swift
 
-/// Prints the current timestamp, formatted for jekyll
+/// Prints the current timestamp, formatted for jekyll.
+/// Copies it to the clipboard.
 
 import Foundation
 
@@ -11,3 +12,23 @@ let fullDateTime = ISO8601DateFormatter.string(
 )
 
 print(fullDateTime)
+
+print("Copying to clipboard...")
+
+let pipe = Pipe()
+
+let echo = Process()
+echo.launchPath = "/usr/bin/env"
+echo.arguments = ["echo", fullDateTime]
+echo.standardOutput = pipe
+
+let pbcopy = Process()
+pbcopy.launchPath = "/usr/bin/env"
+pbcopy.arguments = ["pbcopy"]
+pbcopy.standardInput = pipe
+
+echo.launch()
+pbcopy.launch()
+pbcopy.waitUntilExit()
+
+print("Done!")
