b3acca252b77ef4fabd5db1775178ec42f5d7a01
Jesse Squires <jesse@jessesquires.com>
2021-03-22 14:14:06 -0700

    update date script

--- scripts/current-date-time.swift
+++ scripts/current-date-time.swift
@@ -3,6 +3,7 @@
 /// Prints the current timestamp, formatted for jekyll.
 /// Copies it to the clipboard.
 
+import AppKit
 import Foundation
 
 let fullDateTime = ISO8601DateFormatter.string(
@@ -13,25 +14,7 @@ let fullDateTime = ISO8601DateFormatter.string(
 
 print(fullDateTime)
 
-print("Copying to clipboard...")
+NSPasteboard.general.clearContents()
+NSPasteboard.general.setString(fullDateTime, forType: .string)
 
-let pipe = Pipe()
-
-let echo = Process()
-echo.executableURL = URL(fileURLWithPath: "/usr/bin/env")
-echo.arguments = ["echo", fullDateTime]
-echo.standardOutput = pipe
-
-let pbcopy = Process()
-pbcopy.executableURL = URL(fileURLWithPath: "/usr/bin/env")
-pbcopy.arguments = ["pbcopy"]
-pbcopy.standardInput = pipe
-
-do {
-    try echo.run()
-    try pbcopy.run()
-    pbcopy.waitUntilExit()
-    print("Done!")
-} catch {
-    print("Error: \(error)")
-}
+print("Copied to clipboard!")
