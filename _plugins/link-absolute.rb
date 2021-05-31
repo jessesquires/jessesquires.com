d0bc1765c43867a1b322293beaa36c716c45bf6d
Jesse Squires <jesse@jessesquires.com>
2021-05-30 17:30:04 -0700

    Implement `link_absolute` tag
    Similar to `post_url_absolute` but for links.
    
    ref: #141, #142

---
+++ _plugins/link-absolute.rb
@@ -0,0 +1,23 @@
+# Custom tag to render absolute urls to pages.
+# Similar to `{% link %}` but produces absolute urls.
+#
+# See:
+#    - https://jekyllrb.com/docs/plugins/tags/
+#    - https://github.com/jekyll/jekyll/blob/76517175e700d80706c9139989053f1c53d9b956/lib/jekyll/tags/link.rb
+#    - https://github.com/jekyll/jekyll/blob/76517175e700d80706c9139989053f1c53d9b956/lib/jekyll/filters/url_filters.rb
+
+module Jekyll
+  class LinkAbsolute < Jekyll::Tags::Link
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end
+
+    def render(context)
+      absolute_url("#{ super(context) }")
+    end
+  end
+end
+
+Liquid::Template.register_tag('link_absolute', Jekyll::LinkAbsolute)
